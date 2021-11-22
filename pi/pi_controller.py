a46bd1f6e31a393d42c26099fed42bf471cfcc81
signejohansson <signe.johansson33@gmail.com>
2021-11-22 21:16:24 +0100

    pi_controller ändringar

--- pi/pi_controller.py
+++ pi/pi_controller.py
@@ -1,7 +1,8 @@
 import requests
 import time
 import random
-import click
+from sense_hat import SenseHat
+sense = SenseHat()
 
 
 # Replace with your own function in Part 1
@@ -9,34 +10,36 @@ def get_direction():
     d_long = 0
     d_la = 0
     send_vel = False
-    c = click.getchar()
-    if c =='a':
-        click.echo('Left')
-        send_vel = True
-        d_long = -1
-        d_la = 0
-    elif c == 'd':
-        click.echo('Right')
-        send_vel = True
-        d_long = 1
-        d_la = 0
-    elif c =='w':
-        click.echo('Up')
-        send_vel = True
-        d_long = 0
-        d_la = 1
-    elif c == 's':
-        click.echo('Down')
-        send_vel = True
-        d_long = 0
-        d_la = -1
-    else:
-        d_long = 0
-        d_la = 0
-        click.echo('Invalid input :(')
-        send_vel = False
-    return d_long, d_la, send_vel
+    
+    while True: 
+        for event in sense.stick.get_events():
+            print(event.direction, event.action)
 
+            if event.action == "pressed":
+                
+                if event.direction == "up":
+                    send_vel = True
+                    d_long = 0
+                    d_la = 1
+                elif event.direction == "left":
+                    send_vel = True
+                    d_long = -1
+                    d_la = 0
+                
+                elif event.direction == "right":
+                    send_vel = True
+                    d_long = 1
+                    d_la = 0
+                
+                elif event.direction == "down":
+                    send_vel = True
+                    d_long = 0
+                    d_la = -1
+                else:
+                    d_long = 0
+                    d_la = 0
+                    send_vel = False
+                return d_long, d_la, send_vel
 
 if __name__ == "__main__":
     SERVER_URL = "http://127.0.0.1:5001/drone"
